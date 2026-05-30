277e8b016b24497b4c6078130815a03f0d60300d
komal yadav <komalyadav1.in@gmail.com>
2026-05-30 14:27:48 +0530

    push final project

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const dotenv = require("dotenv");
-
-dotenv.config();
-
+const path = require("path");
+dotenv.config({path: path.join(__dirname, ".env")});
 console.log(process.env.SECRET);
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = 8080;
-const path = require("path");
+
 const methodOverride = require("method-override");
 const Listing = require("./models/listing.js");
 const ExpressError=require("./utils/ExpressError.js");
+const { MongoStore } = require("connect-mongo");
 const ejsMate = require("ejs-mate");
 const listingsRouter=require("./routes/listing.js");
 const reviewsRouter=require("./routes/review.js");
@@ -22,6 +22,8 @@ const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./models/user.js");
 const userRouter=require("./routes/user.js");
+// const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl=process.env.ATLASDB_URL;
 
 
 app.set("view engine", "ejs");
@@ -33,29 +35,13 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 app.use(flash());
 
-app.listen(port, () => {
-    console.log("app is listening");
-});
-
-const sessionOption={
-    secret:"mysupersecretstring",
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        expires:Date.now()+7*24*60*60*1000,
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-    },
-};
-
-app.use(session(sessionOption));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-
+async function main() {
+    await mongoose.connect(dbUrl);
+    console.log("✅ DB Connected!");
+    app.listen(port, () => {
+        console.log("app is listening on port", port);
+    });
+}
 
 main()
     .then(() => {
@@ -65,9 +51,39 @@ main()
         console.log(err);
     });
 
-async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
-}
+const store=MongoStore.create({
+mongoUrl:dbUrl,
+crypto:{
+    secret:process.env.SECRET
+},
+touchAfter:24 * 3600,
+});
+
+store.on("error",(err)=>{
+    console.log("error in mongo session store",err);
+})
+
+const sessionOption={
+    store,
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires:Date.now()+7*24*60*60*1000,
+        maxAge:7*24*60*60*1000,
+        httpOnly:true,
+    },
+};
+
+
+
+app.use(session(sessionOption));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
